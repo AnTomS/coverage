f9a209bdc276136fe5d00dd97c449fa564eeeaa5
Tomilov Algernon Andrey <thekeibee@yandex.ru>
2022-04-22 20:47:18 +0800

    Ready

---
+++ src/test/kotlin/ru/netology/CoverageKtTest.kt
@@ -0,0 +1,35 @@
+package ru.netology
+
+import org.junit.Test
+
+class CoverageKtTest {
+
+    @Test
+    fun transferMastercardMaestro() {
+        //Arrange
+        //Act
+        //Assert
+    }
+
+    @Test
+    fun transferVisaMir() {
+        //Arrange
+        //Act
+        //Assert
+    }
+
+
+    @Test
+    fun moneyTransfer() {
+        //Arrange
+        //Act
+        //Assert
+    }
+
+    @Test
+    fun limitControl() {
+        //Arrange
+        //Act
+        //Assert
+    }
+}

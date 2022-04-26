3691d0297044bf0af73c7a7569a2f0ebf1332815
Tomilov Algernon Andrey <thekeibee@yandex.ru>
2022-04-26 22:29:17 +0800

    check moneyTransfer

--- src/test/kotlin/ru/netology/CoverageKtTest.kt
+++ src/test/kotlin/ru/netology/CoverageKtTest.kt
@@ -11,9 +11,7 @@ class CoverageKtTest {
         //arrange
         val enterTransaction = 5_000_00
         //act
-        val result = transferMastercardMaestro(
-            sumTransactionMastercardMaestro = enterTransaction
-        )
+        val result = transferMastercardMaestro(sumTransactionMastercardMaestro = enterTransaction)
         //assert
         assertEquals(0, result)
     }
@@ -23,9 +21,7 @@ class CoverageKtTest {
         //arrange
         val enterTransaction = 100_000_00
         //act
-        val result = transferMastercardMaestro(
-            sumTransactionMastercardMaestro = enterTransaction
-        )
+        val result = transferMastercardMaestro(sumTransactionMastercardMaestro = enterTransaction)
         //assert
         assertEquals(620, result)
     }
@@ -35,9 +31,7 @@ class CoverageKtTest {
         //arrange
         val enterTransaction = 12_000
         //act
-        val result = transferVisaMir(
-            sumTransactionVisaMir = enterTransaction
-        )
+        val result = transferVisaMir(sumTransactionVisaMir = enterTransaction)
         //assert
         assertEquals(3500, result)
     }
@@ -47,19 +41,17 @@ class CoverageKtTest {
         //arrange
         val enterTransaction = 123_000_00
         //act
-        val result = transferVisaMir(
-            sumTransactionVisaMir = enterTransaction
-        )
+        val result = transferVisaMir(sumTransactionVisaMir = enterTransaction)
+        //assert
         assertEquals(922, result)
     }
 
-   @Test
-   fun moneyTransferMASTERCARD() {
+    @Test
+    fun moneyTransferMASTERCARD() {
         //arrange
         val choice = MASTERCARD
         //act
         val amountKop = moneyTransfer(MASTERCARD)
-
         //assert
         assertEquals(922, amountKop)
     }
@@ -70,37 +62,36 @@ class CoverageKtTest {
         val choice = MAESTRO
         //act
         val amountKop = moneyTransfer(MAESTRO)
-
         //assert
         assertEquals(100, amountKop)
     }
+
     @Test
     fun moneyTransferVISA() {
         //arrange
         val choice = VISA
         //act
         val amountKop = moneyTransfer(VISA)
-
         //assert
         assertEquals(200, amountKop)
     }
+
     @Test
     fun moneyTransferMIR() {
         //arrange
         val choice = MIR
         //act
         val amountKop = moneyTransfer(MIR)
-
         //assert
         assertEquals(400, amountKop)
     }
+
     @Test
     fun moneyTransferVKPAY() {
         //arrange
         val choice = VKPAY
         //act
         val amountKop = moneyTransfer(VKPAY)
-
         //assert
         assertEquals(500, amountKop)
     }
